3aa96c5d15a46d2b2385b91e8fe5e8dc0d62830c
Leandro Eugenio <leoeugenio16@gmail.com>
2023-03-28 11:58:05 -0300

    1.0.7

--- src/inicio/inicio.js
+++ src/inicio/inicio.js
@@ -1,10 +1,25 @@
+import Figure from 'react-bootstrap/Figure';
 import fachada from '../imagenes/INICIO/FACHADA.jpeg'
+import Carousel from 'react-bootstrap/Carousel';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import logo from '../imagenes/LOGO/logo nuevo.png'
+const Inicio = () => {
+    return (
+        <div class="inicio">
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={fachada} />
+                <Card.Body>
+                    <Card.Title>ASPEN CAFETERIA</Card.Title>
+                    <Card.Text>
+                        SOMOS UN LUGAR DESTINADO A PASARLA BIEN
+                    </Card.Text>
+                    <img class="inicio-img" src={logo}/>
+                </Card.Body>
+            </Card>
 
-const Inicio=()=>{
-    return(
-        <div >
-            <img src={fachada}/>
         </div>
+
     )
 }
 
